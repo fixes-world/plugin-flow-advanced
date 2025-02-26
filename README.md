6580511a82f1381eb5dc86bfaf0aafd3cedd6744
Tang Bo Hao <tech@btang.cn>
2025-02-26 23:14:15 +0800

    feat: init plugin-flow-advanced with the codes from ElizaOnFlow

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# plugin-flow-advanced
+# @elizaos-plugins/plugin-flow-advanced
+
 The advanced Eliza plugin for Flow Blockchain
+
+## Description
+
+This plugin provides advanced functionality for the Flow Blockchain. It includes the following features:
+
+- TBD
---
+++ src/helpers/formater.ts
@@ -0,0 +1,15 @@
+import {
+    formatAgentWalletInfo,
+    formatWalletCreated,
+    formatWalletInfo,
+    formatFlowSpent,
+    formatTransationSent
+} from "@elizaos-plugins/plugin-flow";
+
+export {
+    formatAgentWalletInfo,
+    formatWalletCreated,
+    formatWalletInfo,
+    formatFlowSpent,
+    formatTransationSent
+};
